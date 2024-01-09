ff84663368ea40999d1b13ed5360b9a961407d8c
Edwin Siew <edwin@mmpeople.sg.com>
2024-01-09 15:32:05 +0800

    init push

---
+++ public/script/data.js
@@ -0,0 +1,52 @@
+var APP_DATA = {
+  characterCreator: {
+    face: {
+      1: {
+        name: "Head 01",
+        source: "res/models/faces/face_01.gltf",
+        thumbnailSource: "srcURL",
+      },
+    },
+    head: {
+      1: {
+        name: "Hair 01",
+        source: "res/models/heads/head_01.gltf",
+        thumbnailSource: "srcURL",
+      },
+      5: {
+        name: "Hair 05",
+        source: "res/models/heads/head_05.gltf",
+        thumbnailSource: "srcURL",
+      },
+    },
+    eyebrows: {
+      1: {
+        name: "Eyebrows 01",
+        source: "res/models/eyebrows/brows_01.gltf",
+        thumbnailSource: "srcURL",
+      },
+    },
+    shirt: {
+      1: {
+        name: "Shirt 01",
+        source: "res/models/shirts/shirt_01.gltf",
+        thumbnailSource: "srcURL",
+      },
+      2: {
+        name: "Shirt 02",
+        source: "res/models/shirts/shirt_02.glb",
+        thumbnailSource: "srcURL",
+      },
+      4: {
+        name: "Shirt 04",
+        source: "res/models/shirts/shirt_04.glb",
+        thumbnailSource: "srcURL",
+      },
+      5: {
+        name: "Shirt 05",
+        source: "res/models/shirts/shirt_05.glb",
+        thumbnailSource: "srcURL",
+      },
+    },
+  },
+};
